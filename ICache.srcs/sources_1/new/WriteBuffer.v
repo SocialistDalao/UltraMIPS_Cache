528a3f5194b0038bc74722e936da718870d56b75
SocialistDalao <850323576@qq.com>
2020-07-06 18:31:53 +0800

    FIFO add read related operation, but not complete

--- ICache.srcs/sources_1/new/WriteBuffer.v
+++ ICache.srcs/sources_1/new/WriteBuffer.v
@@ -27,8 +27,15 @@ module WriteBuffer(
     input wire cpu_wreq_i,
     input wire [`DataAddrBus]cpu_awaddr_i,
     input wire [`WayBus]cpu_wdata_i,//一个块的大小
+	//CPU read request and response
+    input wire cpu_rreq_i,
+    input wire [`DataAddrBus]cpu_araddr_i,
+	output reg read_hit_o,
+	output reg [`WayBus]cpu_rdata_o,
+	
     //state
     output reg [`FIFOStateBus]state_o,
+	
     //MEM 
     input wire mem_wready_i,
     output wire mem_wvalid_o,
@@ -71,18 +78,25 @@ module WriteBuffer(
     //头尾指针维护
     reg [`FIFONumLog2-1:0]tail;//表征当前正在写入的数据位置
     reg [`FIFONumLog2-1:0]head;//表征最后需要写入数据位置的后一位
+    reg [`FIFONum-1:0]FIFO_valid;//表征buffer中的数据是否有效（高电平有效）
     always@(posedge clk)begin
         if(rst)begin
             head <= `FIFONumLog2'h0;
             tail <= `FIFONumLog2'h0;
+			FIFO_valid <= `FIFONum'h0;
         end
-        else if( mem_bvalid == `Valid)//写入完毕
+        else if( mem_bvalid == `Valid)begin//写入完毕
+			FIFO_valid[head] <= `Invalid;
             head <= head + 1;
-        else if(cpu_wreq_i == `WriteEnable)//增加写入，入队
+		end
+        else if(cpu_wreq_i == `WriteEnable)begin //增加写入，入队
             tail <= tail + 1;
+			FIFO_valid[tail] <= `Valid;
+		end
         else begin
             head <= head;
             tail <= tail;
+			FIFO_valid <= FIFO_valid;
         end
     end
     //数据写入
@@ -94,11 +108,103 @@ module WriteBuffer(
             FIFO_addr[tail] <= cpu_awaddr_i;
         end
     end
+	//读写冲突
+	//读冲突
+	always@(*)begin
+		if(cpu_rreq_i)begin
+			if(cpu_araddr_i == FIFO_addr[0] && FIFO_valid[0])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[0];
+			end
+			if(cpu_araddr_i == FIFO_addr[1] && FIFO_valid[1])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[1];
+			end
+			if(cpu_araddr_i == FIFO_addr[2] && FIFO_valid[2])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[2];
+			end
+			if(cpu_araddr_i == FIFO_addr[3] && FIFO_valid[3])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[3];
+			end
+			if(cpu_araddr_i == FIFO_addr[4] && FIFO_valid[4])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[4];
+			end
+			if(cpu_araddr_i == FIFO_addr[5] && FIFO_valid[5])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[5];
+			end
+			if(cpu_araddr_i == FIFO_addr[6] && FIFO_valid[6])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[6];
+			end
+			if(cpu_araddr_i == FIFO_addr[7] && FIFO_valid[7])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[7];
+			end
+			else begin
+				read_hit_o <= 0;
+				cpu_rdata_o <= 0;
+			end
+		end
+		else begin
+				read_hit_o <= 0;
+				cpu_rdata_o <= 0;
+		end
+	end
+	//写冲突
+	always@(*)begin
+		if(cpu_wreq_i)begin
+			if(cpu_araddr_i == FIFO_addr[0] && FIFO_valid[0])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[0];
+			end
+			if(cpu_araddr_i == FIFO_addr[1] && FIFO_valid[1])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[1];
+			end
+			if(cpu_araddr_i == FIFO_addr[2] && FIFO_valid[2])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[2];
+			end
+			if(cpu_araddr_i == FIFO_addr[3] && FIFO_valid[3])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[3];
+			end
+			if(cpu_araddr_i == FIFO_addr[4] && FIFO_valid[4])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[4];
+			end
+			if(cpu_araddr_i == FIFO_addr[5] && FIFO_valid[5])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[5];
+			end
+			if(cpu_araddr_i == FIFO_addr[6] && FIFO_valid[6])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[6];
+			end
+			if(cpu_araddr_i == FIFO_addr[7] && FIFO_valid[7])begin
+				read_hit_o <= `HitSuccess;
+				cpu_rdata_o <= FIFO_data[7];
+			end
+			else begin
+				read_hit_o <= 0;
+				cpu_rdata_o <= 0;
+			end
+		end
+		else begin
+				read_hit_o <= 0;
+				cpu_rdata_o <= 0;
+		end
+	end
     
     
     //总线处理
     assign mem_bready=1'b1;
-    always@(posedge clk)begin
-        
-    end
+    assign mem_wvalid_o = (state_o == `STATE_EMPTY)? `Invalid:`Valid;
+    assign mem_awvalid_o = (state_o == `STATE_EMPTY)? `Invalid:`Valid;
+    assign mem_awaddr_o = FIFO_addr[head];
+    assign mem_wdata_o = FIFO_data[head];
 endmodule
